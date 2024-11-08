148ecc7c14be2e0361cedd6bafb99cf0fe360d40
mustaphazorkot <mustaphazorkot@gmail.com>
2024-11-08 16:20:35 -0300

    Projeto rpg

---
+++ src/main/java/com/example/rpg_personagem/RpgPersonagemApplication.java
@@ -0,0 +1,13 @@
+package com.example.rpg_personagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgPersonagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgPersonagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rpg_personagem/RpgPersonagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rpg_personagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgPersonagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

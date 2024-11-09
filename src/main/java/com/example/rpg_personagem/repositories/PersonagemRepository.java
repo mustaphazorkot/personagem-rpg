46c479459d4219e10d1b2864fcd67ec85f8af488
mustaphazorkot <mustaphazorkot@gmail.com>
2024-11-09 15:01:51 -0300

    PersonagemRepository update

--- src/main/java/com/example/rpg_personagem/repositories/PersonagemRepository.java
+++ src/main/java/com/example/rpg_personagem/repositories/PersonagemRepository.java
@@ -1,9 +1,13 @@
 package com.example.rpg_personagem.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.rpg_personagem.models.Personagem;
 
 public interface PersonagemRepository extends CrudRepository<Personagem, Long> {
+
+    List<Personagem> findByUser_Id(Long id);
 
 }
